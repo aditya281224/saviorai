125e3b54d50ef37844d87fb75b7ce6fd1eacd6c3
Aditya Sharma <adityasharmavats2811@gmail.com>
2025-08-28 12:12:46 +0530

    interview-Form created

--- app/(main)/mock-interview/mockInterview/create-interview/_components/formContainer.jsx
+++ app/(main)/mock-interview/mockInterview/create-interview/_components/formContainer.jsx
@@ -9,21 +9,55 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { InterviewType } from "@/data/interviewType";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import { useEffect, useState } from "react";
 
-const FormContainer = () => {
+const FormContainer = ({ onHandleInputChange }) => {
+  const [interviewType, setInterviewType] = useState([]);
+  useEffect(() => {
+    if (interviewType) {
+      onHandleInputChange("type", interviewType);
+    }
+  }, [interviewType]);
+
+  const AddInterviewType = (type) => {
+  const exists = interviewType.includes(type);
+  if (!exists) {
+    setInterviewType((prev) => [...prev, type]); 
+  } else {
+    const result = interviewType.filter((item) => item !== type); 
+    setInterviewType(result);
+  }
+};
+
   return (
     <div className="p-5 bg-white">
       <div>
-        <h2 className="text-sm text-black">Job Position</h2>
-        <Input placeholder="Full Stack Developer" className="mt-2 text-blue-950 border-black" />
+        <h2 className="text-sm font-bold text-black">Job Position</h2>
+        <Input
+          placeholder="Full Stack Developer"
+          className="mt-2 text-blue-950 border-black"
+          onChange={(event) =>
+            onHandleInputChange("jobPosition", event.target.value)
+          }
+        />
       </div>
       <div className="mt-5 ">
-        <h2 className="text-sm text-black">Job Description</h2>
-        <Textarea placeholder="Enter Job details" className="h-[200px] mt-2 text-blue-950 border-gray-900" />
+        <h2 className="text-sm text-black font-bold">Job Description</h2>
+        <Textarea
+          placeholder="Enter Job details"
+          className="h-[200px] mt-2 text-blue-950 border-gray-900 "
+          onChange={(event) =>
+            onHandleInputChange("jobDescription", event.target.value)
+          }
+        />
       </div>
       <div className="mt-5">
-        <h2 className="text-sm text-black">Interview Duration</h2>
-        <Select>
+        <h2 className="text-sm text-black font-bold">Interview Duration</h2>
+        <Select
+          onValueChange={(value) => onHandleInputChange("duration", value)}
+        >
           <SelectTrigger className="w-[180px] text-blue-950 border-gray-900">
             <SelectValue placeholder="Select Duration" />
           </SelectTrigger>
@@ -38,18 +72,35 @@ const FormContainer = () => {
       </div>
 
       <div className="mt-5">
-        <h2 className="text-sm text-black">Interview Type</h2>
-        <div>
-          {InterviewType.map((type,index)=>(
-            <div key={index} className="text-black text-sm flex gap-2">
-              <type.icon/>
+        <h2 className="text-sm text-black font-bold">Interview Type</h2>
+        <div className="flex gap-3 flex-wrap mt-2">
+          {InterviewType.map((type, index) => (
+            <div
+              key={index}
+              className={`flex items-center cursor-pointer gap-2 p-1 px-4 
+    border border-gray-300 rounded-2xl 
+    hover:bg-secondary
+    ${
+      interviewType.includes(type.title)
+        ? "bg-blue-50 text-black"
+        : "bg-amber-500"
+    }
+  `}
+              onClick={() => AddInterviewType(type.title)
+              }
+            >
+              <type.icon className="h-6 w-6" />
               <span>{type.title}</span>
             </div>
           ))}
         </div>
       </div>
 
-
+      <div className="mt-7 flex justify-end">
+        <Button>
+          Generate Question<ArrowRight></ArrowRight>
+        </Button>
+      </div>
     </div>
   );
 };
--- app/(main)/mock-interview/mockInterview/create-interview/page.jsx
+++ app/(main)/mock-interview/mockInterview/create-interview/page.jsx
@@ -8,6 +8,15 @@ import FormContainer from './_components/formContainer'
 const page = () => {
   const router = useRouter();
   const [step,setStep]=useState(1);
+  const [formData,setFormData]=useState();
+  const onHandleInputChange = (field, value) => {
+  setFormData(prev => {
+    const updated = { ...prev, [field]: value };
+    console.log("Updated formData:", updated); 
+    return updated;
+  });
+};
+
   return (
     <div className='mt-10 px-10 md:px-24 lg:px-44 xl:px-56'>
       <div className='flex gap-5 items-center'>
@@ -16,7 +25,7 @@ const page = () => {
         
       </div>
       <Progress value={step*33.33} className="my-5"/>
-      <FormContainer/>
+      <FormContainer onHandleInputChange={onHandleInputChange} />
     </div>
   )
 }
